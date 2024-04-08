58a36048ffa2d17ec7a9a51f9cdb87b3191368e0
Porfirev <daniil.porfirev@sulzer.de>
2024-04-08 10:05:02 +0200

    Added the whole functionality of the program. Now if you have a question, then the program will answer it, using three different arrays consisting of introductory, expressive words and contrasting sentences.

---
+++ arrays.py
@@ -0,0 +1,24 @@
+introductory_words = ["absolutely", "unfortunately", "sincerely", "actually", "fortunately",
+                      "honestly", "frankly", "clearly", "indeed", "naturally", "surprisingly",
+                      "admittedly", "arguably", "essentially", "ultimately"]
+
+expressive_words = ["maybe", "I don't know", "no", "yes", "probably", "perhaps",
+                    "I'm not sure", "definitely", "of course", "I guess", "possibly",
+                    "I think so", "not really", "absolutely", "I suppose"]
+
+contrasting_sentences = [
+    "But it may change.",
+    "But it's not my responsibility.",
+    "But I didn't say that to you.",
+    "But I'm not entirely sure.",
+    "But that's not what I meant.",
+    "But I need more time to decide.",
+    "But that was a long time ago.",
+    "But I am under a different impression.",
+    "But things are different now.",
+    "But I can't confirm that yet.",
+    "But you should double-check.",
+    "But I have another commitment.",
+    "But I don't want to jump to conclusions.",
+    "But let's consider the alternatives."
+]
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("my new project")
+from time import sleep
+from random import choice
+import arrays
+
+print("Welcome to the magic ball!")
+name_player = input("What's your name? ")
+print("Hello, ", name_player, "\nSo, what is your 'yes or no' question, my dear friend")
+question = input()
+
+print("Hmmm, let me think")
+sleep(2)
+print(f'{choice(arrays.introductory_words).capitalize()},'
+      f' {choice(arrays.expressive_words)}. {choice(arrays.contrasting_sentences)}')
+sleep(2)
